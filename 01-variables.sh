030603e2e1ca91cd0599881d18a04d3a3ff8afe3
kkrishna0024 <65277456+kkrishna0024@users.noreply.github.com>
2024-11-26 23:07:45 +0530

    updated

--- 01-variables.sh
+++ 01-variables.sh
@@ -8,8 +8,8 @@ read -s person1
 echo "enter 2nd name"
 read -s person2
 
-echo "$person1 , hi $person2 "
-echo "$person2, hi how are you $person1"
-echo " $person1 and $person2"
+echo "$person1:  hi $person2 "
+echo "$person2:  hi $person1, how are you $person1"
+echo "$person1: i am fine  and how are you $person2"
 
-echo "my name is murali"
+echo "end the discussion"
