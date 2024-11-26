88d49603e0bbf5ac7aa58dfa74095c8dfdd277ca
kkrishna0024 <65277456+kkrishna0024@users.noreply.github.com>
2024-11-26 23:04:14 +0530

    updated

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
+
+
+echo "enter 1st name" 
 read -s person1 
+echo "enter 2nd name"
 read -s person2
-person1=$1
-person2=$2
 
 echo "$person1 , hi $person2 "
 echo "$person2, hi how are you $person1"
