48825aa35c22ed6799369942bcfb99a1414dde96
kkrishna0024 <65277456+kkrishna0024@users.noreply.github.com>
2024-11-26 22:59:31 +0530

    new update

---
+++ 01-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+read -s person1 
+read -s person2
+person1=$1
+person2=$2
+
+echo "$person1 , hi $person2 "
+echo "$person2, hi how are you $person1"
+echo " $person1 and $person2"
+
+echo "my name is murali"
