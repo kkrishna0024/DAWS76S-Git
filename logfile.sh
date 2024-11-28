be288727194d13f1a92bb254bd598ca6212bde3f
kkrishna0024 <65277456+kkrishna0024@users.noreply.github.com>
2024-11-28 07:23:04 +0530

    updated

---
+++ logfile.sh
@@ -0,0 +1,33 @@
+ID=$(id -u)
+TIMESTAMP=$(date +$F-$H-$M-$s)
+LOGFILE="/temp/$0-$TIMESTAMP"  
+
+VALIDATE () {
+
+    if [ $1 -ne 0 ]
+    then
+        echo "error:: $2 ... failed"
+        exit 1
+    else 
+        echo " $2... success"
+
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo " Error : : this is not root user"
+    exit 1
+else 
+    echo " you are root user"
+
+fi
+
+yum install mysql -y >>$LOGFILE
+
+VALIDATE $? "installing Mysql"
+
+
+yum install git -y >>$LOGFILE
+
+VALIDATE $? " intalling git"
