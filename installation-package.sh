b46d1ada327b584409f60d68bbcde104da32bd32
kkrishna0024 <65277456+kkrishna0024@users.noreply.github.com>
2024-11-30 13:01:25 +0530

    updated

--- installation-package.sh
+++ installation-package.sh
@@ -4,6 +4,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 VALIDATE () {
 
@@ -24,3 +25,22 @@ then
 else 
     echo -e " $G you are root user"
 fi
+
+echo " all arguments passed $@"
+
+for package in $@
+ do 
+  yum list installed $package 
+
+  if [$? -ne 0]
+   then 
+     yum install $package -y &>>LOGFILE
+     VALIDATE $? "installed  $package $G success"
+    else 
+
+       echo "instlled $package $Y already installed"
+  fi
+
+done
+
+   
