5011e062e7bf8f2b42f4726a607833da839bee79
kkrishna0024 <65277456+kkrishna0024@users.noreply.github.com>
2024-11-30 12:49:29 +0530

    updated

---
+++ installation package.sh
@@ -0,0 +1,26 @@
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"  
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE () {
+
+    if [ $1 -ne 0 ]
+    then
+        echo -e "error:: $2 ... $R failed$N"
+        exit 1
+    else 
+        echo -e " $2... $G success$N"
+
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " Error : :$R this is not root user"
+    exit 1
+else 
+    echo -e " $G you are root user"
+fi
