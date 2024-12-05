c5625038dd5417d24a3add039f76419c80b943a9
kkrishna0024 <65277456+kkrishna0024@users.noreply.github.com>
2024-12-05 09:37:09 +0530

    updated

--- ec2instance.sh
+++ ec2instance.sh
@@ -2,19 +2,39 @@
 
 AMI=ami-0b4f379183e5706b9
 SG_ID=sg-05ea3dc55b6da7ad6
-instances=("Mangodb" "mysql" "redis" "user" "cart" "web" "shipping" "payments" "dispatch" "rabitmq" "catalogue")
+Instances=("Mangodb" "mysql" "redis" "user" "cart" "web" "shipping" "payments" "dispatch" "rabitmq" "catalogue")
 
-for i in "${instances[@]}"
+for i in "${INSTANCES[@]}"
 do 
-  if [ $i== "Mangodb" ] || [$i== "mysql" ] || [ $i== "redis" ]
+  if [ $i == "Mangodb" ] || [$i== "mysql" ] || [ $i== "redis" ]
   then 
-   instance_type=t3.small
+   INSTANCE_TYPE=t3.small
 
    else 
-   instance_type=t2.micro
+   INSTANCE_TYPE=t2.micro
   fi
 
-   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type $instance_type  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress"  --output text)
-   echo "instance is : $i $IP_ADDRESS"
+   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress'  --output text)
+   echo "$i: $IP_ADDRESS"
+
+   aws route53 change-resource-record-sets \
+  --hosted-zone-id Z03864262OMVQMXMFTVWQ \
+  --change-batch '
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'" $i "'.prasuna.site"
+        ,"Type"             : "CA"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'" $IP_ADDRESS "'"
+        }]
+      }
+    }]
+  }
+  
  done
 
+ 
