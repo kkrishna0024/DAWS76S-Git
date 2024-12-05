7865759e1a0c212ffe103575289406b6835b2ca8
kkrishna0024 <65277456+kkrishna0024@users.noreply.github.com>
2024-12-05 09:45:10 +0530

    updated

--- ec2instance.sh
+++ ec2instance.sh
@@ -17,24 +17,24 @@ do
    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress'  --output text)
    echo "$i: $IP_ADDRESS"
 
-   aws route53 change-resource-record-sets \
-  --hosted-zone-id Z03864262OMVQMXMFTVWQ \
-  --change-batch '
-  {
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id Z03864262OMVQMXMFTVWQ \
+    --change-batch "
+   {
     "Comment": "Testing creating a record set"
     ,"Changes": [{
       "Action"              : "CREATE"
       ,"ResourceRecordSet"  : {
-        "Name"              : "'" $i "'.prasuna.site"
-        ,"Type"             : "CA"
+        "Name"              : "$i .prasuna.site"
+        ,"Type"             : "A"
         ,"TTL"              : 1
         ,"ResourceRecords"  : [{
-            "Value"         : "'" $IP_ADDRESS "'"
+            "Value"         : "$IP_ADDRESS"
         }]
       }
     }]
   }
-  
+  "
  done
 
  
